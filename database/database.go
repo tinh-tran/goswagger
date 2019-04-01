973fd29fc40ff9ee5479d652a02ce990dadc4c4c
TinhTran <jackymon41@gmail.com>
2019-04-01 11:42:19 +0700

    update source code

---
+++ database/database.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+	"go_swagger/config"
+	. "go_swagger/models"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+)
+
+var err error
+var settings config.Configuration
+
+func Open() *gorm.DB {
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		settings.Server, settings.User, settings.Pass, settings.Port, settings.Database)
+	db, _ := gorm.Open("mssql", connString)
+
+	if err != nil {
+		fmt.Println(err)
+		defer db.Close()
+	}
+	return db
+}
+
+func Migrate() {
+	settings = config.GetConfig()
+	db := Open()
+	db.AutoMigrate(Person{})
+	defer db.Close()
+}
